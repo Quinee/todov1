67dbac3d52562e619bcbfac0654e4520a44aa282
Atmaram Naik <naik.atmaram@gmail.com>
2023-02-09 14:31:00 +0530

    ATM | added deleteTodo function

--- util/todo.ts
+++ util/todo.ts
@@ -10,3 +10,7 @@ export async function createTodo(request:APIRequestContext,body:{title:string,st
   })
   return {status:resp.status(), body: await resp.json() }
 }
+export async function deleteTodo(request:APIRequestContext,id:number){
+  const resp = await request.delete(`http://144.24.105.148:8080/v1/todo/${id}`)
+  return resp.status()
+}
