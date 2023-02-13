294994ec75b3f81fbdac22f7032236804ad244ef
Gautham Kumaran <42082874+gautham71298@users.noreply.github.com>
2023-02-13 18:56:05 +0530

    Added create and delete tests (#6)
    * GK | added create and delete tests
    
    * GK | incorporated review comments

---
+++ tests/delete.spec.ts
@@ -0,0 +1,29 @@
+import test, { expect } from "@playwright/test";
+import { createTodo, deleteTodo, putTodo } from "../util/todo";
+
+test.describe("Positive test cases", () => {
+  test.beforeEach(async ({ request }, testInfo) => {
+    const { status, body } = await createTodo(request, {
+      title: "Buy Apple shares",
+    });
+    expect(status).toBe(201);
+    expect(body.id).not.toBe(null);
+    testInfo["id"] = body.id;
+  });
+
+  test("Deletion of todo should work if todo exists", async ({
+    request,
+  }, testInfo) => {
+    const status = await deleteTodo(request, testInfo["id"]);
+    expect(status).toBe(200);
+  });
+});
+
+test.describe("Negative test cases", () => {
+  test("Deletion of non existing todo should give 404 via put endpoint", async ({
+    request,
+  }) => {
+    const { status } = await putTodo(request, {}, -1);
+    expect(status).toBe(400);
+  });
+});
