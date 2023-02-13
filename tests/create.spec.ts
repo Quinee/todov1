bb021adb2cff1ea75aa526d893e54c863f6865f0
Gautham <gowthamkumar71298@gmail.com>
2023-02-13 14:32:08 +0530

    GK | added create and delete tests

--- tests/create.spec.ts
+++ tests/create.spec.ts
@@ -1,17 +1,54 @@
 import { test, expect } from "@playwright/test";
 import { createTodo, deleteTodo } from "../util/todo";
 
-test("Creation of todo should work without passing status field", async ({request},testInfo) => {
-  const {status, body} = await createTodo(request,{
-    title:'Bring Milk'
-  })
-  expect(status).toBe(201)
-  expect(body.id).not.toBe(null)
-  expect(body.title).toBe('Bring Milk')
-  testInfo['id'] = body.id
+test.describe.skip("Negative category", () => {
+  test("Creation of todo should work without passing status field", async ({
+    request,
+  }, testInfo) => {
+    const { status, body } = await createTodo(request, {
+      title: "Bring Milk",
+    });
+    expect(status).toBe(201);
+    expect(body.id).not.toBe(null);
+    expect(body.title).toBe("Bring Milk");
+    testInfo["id"] = body.id;
+  });
+
+  test("Creation of todo should work when passed status field", async ({
+    request,
+  }, testInfo) => {
+    const title = "Buy shares";
+    const { status, body } = await createTodo(request, {
+      title: title,
+      status: "ACTIVE",
+    });
+    expect(status).toBe(201);
+    expect(body.id).not.toBe(null);
+    expect(body.title).toBe(title);
+    testInfo["id"] = body.id;
+  });
+
+  test.afterEach(async ({ request }, testInfo) => {
+    const id = testInfo["id"];
+    const status = await deleteTodo(request, id);
+    expect(status).toBe(200);
+  });
 });
-test.afterEach(async ({request},testInfo)=>{
-  const id = testInfo['id']
-  const status = await deleteTodo(request,id)
-  expect(status).toBe(200)
-})
+
+test.describe("Negative category", () => {
+  test("Creation of Todo should give 400 when title field is not passed", async ({
+    request,
+  }) => {
+    const { status } = await createTodo(request, {});
+    expect(status).toBe(400);
+  });
+
+  test("Creation of todo should give 400 when status value is not eaither ACTIVE or DONE", async ({
+    request,
+  }) => {
+    const { status } = await createTodo(request, {
+      status: "DONE",
+    });
+    expect(status).toBe(400);
+  });
+});
--- util/todo.ts
+++ util/todo.ts
@@ -1,6 +1,6 @@
 import { APIRequestContext } from "@playwright/test";
 
-export async function createTodo(request:APIRequestContext,body:{title:string,status?:string}){
+export async function createTodo(request:APIRequestContext,body:{title?:string,status?:string}){
     const resp = await request.post('http://144.24.105.148:8080/v1/todo',{
     data:body,
     headers:{
