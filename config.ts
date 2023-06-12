fbd6b4b56f120fba7baf1c96cc47145118e32a3d
Atmaram Naik <atmaram.naik@sedintechnologies.com>
2023-06-12 13:05:23 +0530

    config

---
+++ config.ts
@@ -0,0 +1,1 @@
+export const BASE_URL=process.env.BASE_URL??'http://144.24.105.148:8080'
--- util/todo.ts
+++ util/todo.ts
@@ -1,7 +1,8 @@
 import { APIRequestContext } from "@playwright/test";
+import { BASE_URL } from "../config";
 
 export async function createTodo(request:APIRequestContext,body:{title?:string,status?:string}){
-    const resp = await request.post('http://144.24.105.148:8080/v1/todo',{
+    const resp = await request.post(`${BASE_URL}/v1/todo`,{
     data:body,
     headers:{
       'Content-Type':'application/json'
@@ -11,13 +12,13 @@ export async function createTodo(request:APIRequestContext,body:{title?:string,s
   return {status:resp.status(), body: await resp.json() }
 }
 export async function deleteTodo(request:APIRequestContext,id:number){
-  const resp = await request.delete(`http://144.24.105.148:8080/v1/todo/${id}`)
+  const resp = await request.delete(`${BASE_URL}/v1/todo/${id}`)
   return resp.status()
 }
 
 export async function patchTodo(request:APIRequestContext,body:{title?:string,status?:string},id:number)
 {
-  const resp = await request.patch(`http://144.24.105.148:8080/v1/todo/${id}`,{
+  const resp = await request.patch(`${BASE_URL}/v1/todo/${id}`,{
     data:body,
     headers:{
       'Content-Type':'application/json'
@@ -29,7 +30,7 @@ export async function patchTodo(request:APIRequestContext,body:{title?:string,st
 
 export async function putTodo(request:APIRequestContext,body:{title?:string,status?:string},id:number)
 {
-  const resp=await request.put(`http://144.24.105.148:8080/v1/todo/${id}`,{
+  const resp=await request.put(`${BASE_URL}/v1/todo/${id}`,{
     data:body,
     headers:{
       'Content-Type':'application/json'
@@ -40,11 +41,11 @@ export async function putTodo(request:APIRequestContext,body:{title?:string,stat
 }
 
 export async function getTodo(request:APIRequestContext,id:number){
-  const resp = await request.get(`http://144.24.105.148:8080/v1/todo/${id}`)
+  const resp = await request.get(`${BASE_URL}/v1/todo/${id}`)
   return {status:resp.status(), body: await resp.json()}
 }
 
 export async function getTodoAll(request:APIRequestContext){
-  const resp = await request.get(`http://144.24.105.148:8080/v1/todo`)
+  const resp = await request.get(`${BASE_URL}/v1/todo`)
   return {status:resp.status(), body: await resp.json()}
 }
